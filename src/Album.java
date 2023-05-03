bd87bedc6b706d283607e187b547735630c0495e
Ankush <ankushpaulclg2002@gmail.com>
2023-05-04 01:55:58 +0530

    final commit

--- src/Album.java
+++ src/Album.java
@@ -1,13 +1,79 @@
 import java.util.*;
 
 public class Album {
-    private String name;
-    private String artist;
+    public String name;
+    public String artist;
     private ArrayList<Song> songs;
 
-    public Album(String name, String artist, ArrayList<Song> songs) {
+    public Album(String name, String artist) {
         this.name = name;
         this.artist = artist;
         this.songs = new ArrayList<>();
     }
+
+    /**
+     * 
+     * @param title
+     * @return wether it is in the songs list or not
+     */
+    public boolean findSong(String title) {
+        for (Song song : songs) {
+            if(song.getTitle().equals(title)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Add song to the songs list
+     * @param title of the song
+     * @param duration of the song
+     * @return true if added neither false
+     */
+    public boolean addSong(String title, double duration) {
+        if(!findSong(title)) {
+            songs.add(new Song(title, duration));
+            System.out.println(title + " Successfully added to the list.");
+            return true;
+        } else {
+            System.out.println("Song with this title: " + title + " Already exist in the list.");
+            return false;
+        }   
+    }
+
+    //? Method overloading
+    /**
+     * Add a song to the playlist
+     * @param trackNumber of the song
+     * @param playList of songs
+     * @return true if added neither false
+     */
+    public boolean addToPlayList(int trackNumber, LinkedList<Song> playlist) {
+        int index = trackNumber-1;
+        if(index > 0 && index <= this.songs.size()) {
+            playlist.add(this.songs.get(index));
+            return true;
+        } 
+        System.out.println("This album doesn't have song with the trackNumber: " + trackNumber);
+        return false;
+    }
+
+    /**
+     * Add a song to the playlist
+     * @param title of the song
+     * @param playlist of songs
+     * @return true if added neither false
+     */
+    public boolean addToPlayList(String title, LinkedList<Song> playlist) {
+        for (Song song : this.songs) {
+            if(song.getTitle().equals(title)) {
+                playlist.add(song);
+                return true;
+            }
+        }
+        System.out.println("There is no such a song with the title: " + title + " in the album!");
+        return false;
+    }
+
 }
--- src/App.java
+++ src/App.java
@@ -1,6 +1,141 @@
+import java.util.*;
+
 public class App {
+
+    private static ArrayList<Album> albums = new ArrayList<>();
+    static Scanner sc = new Scanner(System.in);
+
+    private static void play(LinkedList<Song> playlist) {
+        boolean quit = false;
+        boolean forward = true;
+        ListIterator<Song> listIterator = playlist.listIterator();
+
+        if (playlist.size() == 0) {
+            System.out.println("This playlist has no song!");
+        } else {
+            System.out.println("Now playing: " + listIterator.next().toString());
+            printMenu();
+        }
+
+        while (!quit) {
+            int action = sc.nextInt();
+            sc.nextLine();
+
+            switch (action) {
+                case 0:
+                    System.out.println("Playlist complete");
+                    quit = true;
+                    break;
+                case 1:
+                    if (!forward) {
+                        if (listIterator.hasNext()) {
+                            listIterator.next();
+                        }
+                        forward = true;
+                    }
+                    if (listIterator.hasNext()) {
+                        System.out.println("Now playing: " + listIterator.next().toString());
+                    } else {
+                        System.out.println("There is no next song available!");
+                        forward = false;
+                    }
+                    break;
+                case 2:
+                    if (forward) {
+                        if (listIterator.hasPrevious()) {
+                            listIterator.previous();
+                            forward = false;
+                        }
+                    }
+                    if (listIterator.hasPrevious()) {
+                        System.out.println("Now playing: " + listIterator.previous().toString());
+
+                    } else {
+                        System.out.println("We are at the first song!");
+                        forward = false;
+                    }
+                    break;
+                case 3:
+                    if (forward) {
+                        if (listIterator.hasPrevious()) {
+                            System.out.println("Now playing: " + listIterator.previous().toString());
+                            forward = false;
+                        } else {
+                            System.out.println("We are at the start of the playlist!");
+                        }
+                    } else {
+                        if (listIterator.hasNext()) {
+                            System.out.println("Now playing: " + listIterator.next().toString());
+                            forward = true;
+                        } else {
+                            System.out.println("We have reached at the end playlist!");
+                        }
+                    }
+                    break;
+                case 4:
+                    printList(playlist);
+                    break;
+                case 5:
+                    printMenu();
+                    break;
+                case 6:
+                    if (playlist.size() > 0) {
+                        listIterator.remove();
+                        if (listIterator.hasNext()) {
+                            System.out.println("Now playing: " + listIterator.next().toString());
+                        } else {
+                            if (listIterator.hasPrevious()) {
+                                System.out.println("Now playing: " + listIterator.previous().toString());
+
+                            }
+                        }
+                    }
+                default:
+                    break;
+            }
+        }
+    }
+
+    private static void printMenu() {
+        System.out.println("Available options press -> ");
+        System.out.println(
+                "0 -> to quit!\n" + "1 -> to play next song\n" + "2 -> to play previous song\n"
+                        + "3 -> to replay the current song\n" + "4 -> list of all songs\n"
+                        + "5 -> print all available options\n" + "6 -> delete the current song.");
+    }
+
+    public static void printList(LinkedList<Song> playlist) {
+        Iterator<Song> iterator = playlist.iterator();
+        System.out.println("---------------");
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+        System.out.println("---------------");
+    }
+
     public static void main(String[] args) throws Exception {
-        Song song = new Song("Wada Raha",5.49);
-        System.out.println(song);
+
+        Album album = new Album("90's hit songs", "Kishor Kumar");
+
+        album.addSong("Tu Tu Hai Wahi", 4.32);
+        album.addSong("Neele Neele Amabar Par", 5.12);
+        album.addSong("Wada Karo", 6.23);
+        album.addSong("Pal Pal Dil K Paas", 4.36);
+        albums.add(album);
+
+        album = new Album("Romantic songs", "Arijit Singh");
+
+        album.addSong("Hawayein", 3.56);
+        album.addSong("Samjhanwa", 5.22);
+        albums.add(album);
+
+        LinkedList<Song> playlist1 = new LinkedList<>();
+        albums.get(0).addToPlayList("Tu Tu Hai Wahi", playlist1);
+        albums.get(0).addToPlayList("Wada Karo", playlist1);
+        albums.get(1).addToPlayList("Hawayein", playlist1);
+        albums.get(1).addToPlayList("Samjhanwa", playlist1);
+
+        play(playlist1);
+
     }
 }
